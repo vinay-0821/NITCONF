741550467e0013e0eecc0e160588065710109825
Vinay <cvinay2108@gmail.com>
2024-02-05 00:23:58 +0530

    dashboard changed

--- reviewer/src/main/java/reviewer/data/ReviewRepository.java
+++ reviewer/src/main/java/reviewer/data/ReviewRepository.java
@@ -1,8 +1,11 @@
 package reviewer.data;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository {
+import reviewer.model.Review;
 
+@Repository
+public interface ReviewRepository extends JpaRepository<Review,Long>{
+	Review findReviewById(Long Id);
 }
