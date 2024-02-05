698198dfdbfc4b56c294d76f94bb269e81ff950b
8xor9abhiram <varnt2417@gmail.com>
2024-02-05 20:08:45 +0530

    Backend update

--- reviewer/src/main/java/reviewer/data/ReviewRepositoy.java
+++
@@ -1,5 +0,0 @@
-package reviewer.data;
-
-public class ReviewRepositoy {
-
-}
--- reviewer/src/main/java/reviewer/model/Review.java
+++ reviewer/src/main/java/reviewer/model/Review.java
@@ -1,11 +1,13 @@
 package reviewer.model;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import reviewer.util.ReviewForm;
 import reviewer.util.ReviewKey;
 
 
@@ -19,6 +21,15 @@ public class Review {
 	
 	//add attribute for review entity
 	
+	@Column(nullable=true)
+	private Integer originality;
+	
+	@Column(columnDefinition = "varchar(255) default 'accept'")
+	private String reviewerStatus;
+	
+	@Column(columnDefinition = "varchar(255) default 'new'")
+	private String reviewStatus;
+	
 	
 	@ManyToOne 
 	@MapsId("userId")
@@ -30,7 +41,81 @@ public class Review {
 	@MapsId("paperId")
 	@JoinColumn(name="paper_id")
 	Paper paper;
+
+
+	public ReviewKey getId() {
+		return id;
+	}
+
+
+	public void setId(ReviewKey id) {
+		this.id = id;
+	}
+
+
+	public Integer getOriginality() {
+		return originality;
+	}
+
+
+	public void setOriginality(Integer originality) {
+		this.originality = originality;
+	}
+
+
+	public String getReviewerStatus() {
+		return reviewerStatus;
+	}
+
+
+	public void setReviewerStatus(String reviewerStatus) {
+		this.reviewerStatus = reviewerStatus;
+	}
+
+
+	public String getReviewStatus() {
+		return reviewStatus;
+	}
+
+
+	public void setReviewStatus(String reviewStatus) {
+		this.reviewStatus = reviewStatus;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public Paper getPaper() {
+		return paper;
+	}
+
+
+	public void setPaper(Paper paper) {
+		this.paper = paper;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", originality=" + originality + ", reviewerStatus=" + reviewerStatus
+				+ ", reviewStatus=" + reviewStatus + ", user=" + user + ", paper=" + paper + "]";
+	}
+
 	
+	public void modifyReview(ReviewForm reviewFrom)
+	{
+		 this.reviewStatus = "draft";
+		 this.originality = reviewFrom.getOriginality();
+	}
+
+
 	
-	
-} 
+}
