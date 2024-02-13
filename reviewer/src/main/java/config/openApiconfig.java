9bac99225206535b2bd67b4d9a7fcbf41e159452
8xor9abhiram <varnt2417@gmail.com>
2024-02-13 07:27:12 +0530

    config openapi,swagger

--- reviewer/src/main/java/config/openApiconfig.java
+++ reviewer/src/main/java/config/openApiconfig.java
@@ -1,5 +1,34 @@
 package config;
 
 public class openApiconfig {
+        /*
+         * sample openapiconfig
+         * 
+					tag up@Configuration
+					tag up@EnableSwagger2
 
+				    @Bean
+				    public Docket api() {
+				        return new Docket(DocumentationType.SWAGGER_2)
+				                .select()
+				                .apis(RequestHandlerSelectors.basePackage("your.package.controller"))
+				                .paths(PathSelectors.any())
+				                .build()
+				                .apiInfo(apiInfo());
+				    }
+				
+				    
+				    private ApiInfo apiInfo() {
+				        return new ApiInfoBuilder()
+				                .title("Your API Title")
+				                .description("Description of your API")
+				                .version("1.0.0")
+				                .build();
+				    }
+
+         * 
+         * 
+         * 
+         * 
+         */
 }
